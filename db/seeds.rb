24bd21c7c718dea4ab2a4c2f048760ab3f5c577a
Paweł Szyszka <pawelszyszka26@wp.pl>
2020-03-11 21:50:34 +0100

    Adds basic changes to Gemfile, seeds, database.yml, test.rb, sidekiq.yml, rails and spec helpers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-require "faker"
+require 'faker'
 
 Rails.logger = Logger.new(STDOUT)
 
-Rails.logger.info "Creating users..."
+Rails.logger.info 'Creating users...'
 
 20.times do |i|
-  number = i.zero? ? "" : i + 1
+  number = i.zero? ? '' : i + 1
   name = "user#{number}"
   email = "#{name}@example.com"
   next if User.exists?(email: email)
@@ -13,11 +13,11 @@ Rails.logger.info "Creating users..."
     email: email,
     name: name,
     confirmed_at: Time.zone.now,
-    password: "password"
+    password: 'password'
   )
 end
 
-Rails.logger.info "Creating genres..."
+Rails.logger.info 'Creating genres...'
 
 %w(Action Comedy Sci-Fi War Crime
    Horror Sport Western Drama
@@ -27,48 +27,48 @@ end
 
 movies = [
   {
-    title: "Pulp Fiction",
-    release_year: "1994"
+    title: 'Pulp Fiction',
+    release_year: '1994'
   },
   {
-    title: "Django",
-    release_year: "2012"
+    title: 'Django',
+    release_year: '2012'
   },
   {
-    title: "Kill Bill",
-    release_year: "2003"
+    title: 'Kill Bill',
+    release_year: '2003'
   },
   {
-    title: "Kill Bill 2",
-    release_year: "2004"
+    title: 'Kill Bill 2',
+    release_year: '2004'
   },
   {
-    title: "Inglourious Basterds",
-    release_year: "2009"
+    title: 'Inglourious Basterds',
+    release_year: '2009'
   },
   {
-    title: "Godfather",
-    release_year: "1972"
+    title: 'Godfather',
+    release_year: '1972'
   },
   {
-    title: "The Dark Knight",
-    release_year: "2008"
+    title: 'The Dark Knight',
+    release_year: '2008'
   },
   {
-    title: "Star Wars V",
-    release_year: "1980"
+    title: 'Star Wars V',
+    release_year: '1980'
   },
   {
-    title: "Inception",
-    release_year: "2010"
+    title: 'Inception',
+    release_year: '2010'
   },
   {
-    title: "Deadpool",
-    release_year: "2016"
+    title: 'Deadpool',
+    release_year: '2016'
   }
 ]
 
-Rails.logger.info "Creating movies..."
+Rails.logger.info 'Creating movies...'
 
 genre_ids = Genre.pluck(:id)
 if Movie.count < 100
