1693be1642684799e74fc5088893fcbf05475f1e
Paweł Szyszka <pawelszyszka26@wp.pl>
2020-03-11 22:29:13 +0100

    Creates new methods used in views in application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def avatar_or_cover(movie)
+    movie&.avatar.present? ? movie.avatar : movie.cover
+  end
+
+  def is_model_instance?(model, model_name)
+    model.is_a?(model_name)
+  end
 end
